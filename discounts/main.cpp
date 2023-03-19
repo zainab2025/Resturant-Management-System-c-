8d44ad048b3a216ca33fdb6e6e0ea439b0e65a2f
Zainab Hossien <zainb.1718150@stemismailia.moe.edu.eg>
2023-03-19 11:36:38 +0200

    edit on read record function

--- discounts/main.cpp
+++ discounts/main.cpp
@@ -50,12 +50,18 @@ void display_Product_Discount(){
     file.open("product_discounts.txt",ios::in);
     productDiscount d;
     if(file.is_open()){
+        int id_product=0;
         while (!file.eof()){
             file.read((char*)&d,sizeof(d));
-            cout<<"product: "<<d.Product_name<<'\t'
+            if (id_product != d.Product_id){
+                cout<<"product: "<<d.Product_name<<'\t'
             <<"Amount of disccount: "<<d.Discount_amount<<'\t'
             <<"from: "<<d.start_date<<'\t'
             <<"to: "<<d.end_date<<'\t'<<endl;
+
+            id_product=d.Product_id;
+            }
+
         }
     }else {
         cout<<"Ooops!...We Cannot access file"<<endl;
